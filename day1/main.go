d0a68a76a0bf1519d0d48b824113753e32c0d78f
Sean Wong <wotzhs@gmail.com>
2019-12-05 19:21:04 +0800

    master: complete day1 part 2

--- day1/main.go
+++ day1/main.go
@@ -9,6 +9,11 @@ import (
 )
 
 func main() {
+	part1()
+	part2()
+}
+
+func part1() {
 	data, err := ioutil.ReadFile("./input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -24,5 +29,38 @@ func main() {
 		}
 		fuelRequirement = fuelRequirement + (intMass/3 - 2)
 	}
-	fmt.Println(fuelRequirement)
+	fmt.Printf("part 1 solution: %d\n", fuelRequirement)
+}
+
+func part2() {
+	data, err := ioutil.ReadFile("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	input := strings.Split(string(data), "\n")
+
+	var fuelRequirement int
+	for _, mass := range input {
+		intMass, err := strconv.Atoi(mass)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fuelRequirement = fuelRequirement + calculateRequiredFuel(intMass)
+	}
+	fmt.Printf("part 2 solution: %d\n", fuelRequirement)
+}
+
+func calculateRequiredFuel(mass int) int {
+	requiredFuel := mass/3 - 2
+
+	if requiredFuel > 0 {
+		requiredFuel = requiredFuel + calculateRequiredFuel(requiredFuel)
+	}
+
+	if requiredFuel < 0 {
+		return 0
+	}
+
+	return requiredFuel
 }
