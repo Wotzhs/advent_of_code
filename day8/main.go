31aa2b5acb55e7cfce4da7a30fed44e5e09688a4
Sean Wong <wotzhs@gmail.com>
2019-12-20 17:51:58 +0800

    master: complete day8 part2

--- day8/main.go
+++ day8/main.go
@@ -6,7 +6,17 @@ import (
 	"strings"
 )
 
+const (
+	width  int = 25
+	height int = 6
+)
+
 func main() {
+	part1()
+	part2()
+}
+
+func part1() {
 	f, err := ioutil.ReadFile("./input.txt")
 	if err != nil {
 		panic(err)
@@ -17,7 +27,7 @@ func main() {
 	largestBalance := ""
 
 	for len(inputs) > 0 {
-		size := 25 * 6
+		size := width * height
 		if len(inputs) < size {
 			size = len(inputs)
 		}
@@ -44,5 +54,57 @@ func main() {
 		total *= count
 	}
 
-	fmt.Printf("total %v\n", total)
+	fmt.Printf("part 1 total: %v\n", total)
+}
+
+func part2() {
+	f, err := ioutil.ReadFile("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	inputs := string(f)
+
+	image := []string{}
+
+	for len(inputs) > 0 {
+		size := width * height
+		if len(inputs) < size {
+			size = len(inputs)
+		}
+
+		currentGroup := inputs[:size]
+		image = append(image, currentGroup)
+
+		inputs = inputs[size:]
+	}
+
+	visual := [height][width]string{}
+	for row, line := range visual {
+		for column, _ := range line {
+			for _, layer := range image {
+				if string(layer[column+row*width]) != "2" {
+					visual[row][column] = string(layer[column+row*width])
+					break
+				}
+			}
+		}
+	}
+
+	fmt.Println("part 2 answer:")
+	fmt.Println("")
+
+	for _, line := range visual {
+		for _, pixel := range line {
+			if pixel == "0" {
+				fmt.Printf("%v", " ")
+			}
+			if pixel == "1" {
+				fmt.Printf("%v", "x")
+			}
+		}
+		fmt.Println("")
+	}
+
+	fmt.Println("")
 }
